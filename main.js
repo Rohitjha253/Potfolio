5d0b7199eeb9aa3e2b3fe1ba3a32b792f4061cf3
Rohit Jha <jharohit25@gmail.com>
2023-03-29 10:42:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert("hello");
+const myForm=document.getElementById('my-form');
+const namein=document.getElementById('uname');
+const email=document.getElementById('uemail');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e)
+{
+    e.preventDefault();
+
+    if(namein.value=== '' || email.value==='')
+    {
+        alert("Enter required feilds");
+    }
+
+}
